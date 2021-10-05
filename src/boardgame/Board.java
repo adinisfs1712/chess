89cc965cf437f28c83436f4344cc80c02bbc2c19
adinis1712 <dinis.ferreira.souza@gmail.com>
2021-10-05 19:47:12 -0300

    chess layer

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import boardgame.Position;
+
 public class Board {
 	
 	private int rows;
@@ -29,7 +31,14 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+
 	
 
 }
